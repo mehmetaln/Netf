af05c38cd877e4e69cc75127e48ca5fc73f0bd48
Ali ALİN <mehmettalnn@gmail.com>
2023-12-24 21:42:08 +0300

    alt yapı hazırlandı

--- Netf/settings.py
+++ Netf/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appMy',
+    'appUser',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
