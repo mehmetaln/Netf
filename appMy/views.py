76a13c36c90b71f50a5ef0ed57e20a5a7c93bf8e
Ali ALİN <mehmettalnn@gmail.com>
2023-12-25 10:15:15 +0300

    a

--- appMy/views.py
+++ appMy/views.py
@@ -13,3 +13,18 @@ def browsePage(request):
 def browse_indexPage(request):
     context = {}
     return render(request, "browse-index.html", context)
+
+
+
+def hesapPage(request):
+    context = {}
+    return render(request, "user/hesap.html", context)
+
+
+def loginPage(request):
+    context = {}
+    return render(request,"user/login.html", context)
+
+def videoPage(request):
+    context ={}
+    return render(request,"user/video.html", context)
