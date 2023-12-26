7609becc43a955148e8dfcce3dc555b417e406eb
Ali ALİN <mehmettalnn@gmail.com>
2023-12-26 14:16:54 +0300

    loginpage bitti

--- appUser/views.py
+++ appUser/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -19,6 +20,28 @@ def videoPage(request):
 
 
 def loginPage(request):
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password =request.POST.get("password")
+        remember = request.POST.get("rememberme") 
+        
+        if remember:
+            request.session.set_expiry(1209600)
+        else:
+            request.session.set_expiry(0)
+            
+        
+        user =authenticate(username = username, password = password)
+        if user:
+            login(request, user)
+            messages.success(request, "Girişiniz Yapıldı")
+            return redirect('profilPage')
+        else:
+            messages.error(request,"Kullanıcı adınız veya Şifreniz yanlış")
+            return redirect("loginPage")
+            
+    
     
     context = {}
     return render(request,"user/login.html", context)
